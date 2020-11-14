8d7f4b4c984560bf77607be65a8671c1008b8128
snowapril <sinjihng@naver.com>
2020-11-14 08:15:16 +0000

    "*Data Types*" tutorial done.

--- tutorial/04-common-programming-concepts/src/main.rs
+++ tutorial/04-common-programming-concepts/src/main.rs
@@ -17,4 +17,47 @@ fn main() {
 	let spaces = "     ";
 	let spaces = spaces.len();
 	println!("#spaces {}", spaces);
+	
+	let guess: u32 = "42".parse().expect("NAN!");
+	// In rust, integer is by default i32 type.
+	let number = 100; 
+	
+	let mut number: u8 = 250;
+	// number += 10; -> in debug mode, compile error occur. "attempt to compute `250_u8 + 10_u8` which would overflow"
+	
+	// In rust, floating point is f64 by default.
+	let number = 32.103; 
+	let number: f32 = 32.103;
+	
+	let t = true;
+
+    let f: bool = false; // with explicit type annotation
+	
+	// In rust, char is 4bytes by default for representing Unicode Scalar Types.
+	let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '😻';
+	println!("{}", heart_eyed_cat);
+	
+	// Compound type, tuple.
+	let tuple: (i32, f64, u8) = (500, 6.4, 1);
+	let (x, y, z) = tuple;
+	println!("y is {}", y);
+	
+	let middle = tuple.1;
+	println!("tuple getter {}", middle);
+	
+	// Compound type, array.
+	// In rust, array have fixed length.
+	let arr = [1, 2, 3, 4, 5];
+	
+	let arr: [i32; 5] = [1, 2, 3, 4, 5];	
+	let initialized_arr = [3; 5]; // == [3, 3, 3, 3, 3]
+	println!("First element of the initialized_arr = {}", initialized_arr[0]);
+	
+	another_function();
+}
+
+fn another_function() {
+	println!("Hello");
 }
