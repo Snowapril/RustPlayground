e2b3cd34e0b9679c10f326b1bf692c068acaddb5
snowapril <sinjihng@naver.com>
2020-11-21 06:05:21 +0000

    struct and method done

--- tutorial/07-rectangle/src/main.rs
+++ tutorial/07-rectangle/src/main.rs
@@ -4,18 +4,36 @@ struct Rectangle {
     height: u32
 }
 
+// define method
+impl Rectangle {
+    // method
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    // method
+    fn can_hold(&self, other: Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
+    // associated functions. it is function not method
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size
+        }
+    }
+}
+
 fn main() {
     let rect = Rectangle {
         width: 30,
         height: 100
     };
     
-    println!("Area of the rectangle is {}", area(&rect));
+    println!("Area of the rectangle is {}", rect.area());
 
     // the specifier :? inside the curly brackets tells println! we want to use an output format called Debug
     println!("Area of the rectangle is {:#?}", rect);
-}
 
-fn area(rect: &Rectangle) -> u32 {
-    rect.width * rect.height
+    let squared_rect = Rectangle::square(10);
+    println!("Squared rectangle info : {:#?}", squared_rect);
 }
