b7ec92ea6e82343f9896a067bea358241f6741f4
snowapril <sinjihng@naver.com>
2021-02-04 21:00:22 +0900

    bringing paths with use keyword and separating modules into multi files

--- tutorial/09-organizing-project/src/lib.rs
+++ tutorial/09-organizing-project/src/lib.rs
@@ -1,52 +1,24 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+mod front_of_house;
+mod back_of_house;
 
-        fn seat_at_table() {}
-    }
+// below both are do same thing.
+// use crate::front_of_house::hosting;
+pub use self::front_of_house::hosting;
 
-    pub mod serving {
-        fn take_order() {}
-
-        pub fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
-
-mod back_of_house {
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peach"),
-            }
-        }
-    }
-
-    pub enum Appetizer {
-        Soup,
-        Salad,
-    }
-
-    fn fix_incorrect_order() {
-        cook_order();
-
-        super::front_of_house::serving::serve_order();
-    }
-
-    fn cook_order() {}
-}
+// it is idiomatic not to bring whole path of the function
+// but it is for the struct, enums and the others.
+use std::collections::HashMap;
 
 pub fn eat_at_restaurant() {
+
+    // Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
 
+    // Relative path
     front_of_house::hosting::add_to_waitlist();
+
+    // Path aliasing with use keyword
+    hosting::add_to_waitlist();
 
     let mut meal = back_of_house::Breakfast::summer("Rye");
     meal.toast = String::from("Wheat");
@@ -54,4 +26,18 @@ pub fn eat_at_restaurant() {
 
     let order1 = back_of_house::Appetizer::Soup;
     let order2 = back_of_house::Appetizer::Salad;
+
+    let mut map = HashMap::new();
+    map.insert(1, 2);
 }
+
+// use std::fmt::Result;
+// use std::io::Result as IOResult;
+
+// fn function1() -> Result {
+
+// }
+
+// fn function2() -> IOResult<()> {
+
+// }
--- tutorial/09-organizing-project/src/main.rs
+++ tutorial/09-organizing-project/src/main.rs
@@ -1,3 +1,15 @@
+use rand::Rng;
+
+// Nested paths
+use std::{cmp::Ordering, io};
+// use std::io::{self, Write};
+
+// glob operator
+use std::collections::*;
+
 fn main() {
     println!("Hello, world!");
+    
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("Secret number is {}", secret_number);
 }
