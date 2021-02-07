0e61823d6ab576a850aca152e9e10934084beace
snowapril <sinjihng@naver.com>
2021-02-07 18:52:16 +0900

    String and str types done

--- tutorial/10-common-collections/src/main.rs
+++ tutorial/10-common-collections/src/main.rs
@@ -70,4 +70,106 @@ fn main() {
             _ => println!("Otherwise")
         };
     }
+
+
+    let mut s = String::new();
+
+    let data = "initial contents";
+    // to_string method which is available on any type that implements the Display trait.
+    let s = data.to_string();
+    let s = "initial contents".to_string();
+    let s = String::from("initial contents");
+
+    // String is UTF8 encoded.
+    let hello = String::from("السلام عليكم");
+    let hello = String::from("Dobrý den");
+    let hello = String::from("Hello");
+    let hello = String::from("שָׁלוֹם");
+    let hello = String::from("नमस्ते");
+    let hello = String::from("こんにちは");
+    let hello = String::from("안녕하세요");
+    let hello = String::from("你好");
+    let hello = String::from("Olá");
+    let hello = String::from("Здравствуйте");
+    let hello = String::from("Hola");
+
+    let mut s = String::from("foo");
+    s.push_str("bar");
+
+    let mut s = String::from("foo");
+    let s2 = "bar";
+    // push_str does not care about ownership of the parameter.
+    // thus s2 is still available after calling push_str.
+    s.push_str(s2);
+    println!("s2 is {}", s2);
+
+    let mut s = String::from("lo");
+    s.push('l');
+
+    // concatenation with the + operator or the format! macro.
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("World!");
+    let s3 = s1 + &s2; // note that ownership of the s1 is moved here and no longer be used.
+    // println!("s1 is not available {}", s1);
+    println!("s1 + s2 is {}", s3);
+
+    // + operator use add method which looks like below.
+    // fn add(self, s: &str) -> String {}
+    // s1 is self here and &s2 is &String
+    // compiler do coerce the &String into &str.
+    // when we call add method, rust uses `deref coercion`
+    // which turns &s2 into &s2[..]
+
+    // copy is occurred in add method once.
+    // s1 is move ownership to the function 
+    // `copy` contents of the s2 
+    // return ownership of the result.
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = s1 + "-" + &s2 + "-" + &s3;
+    println!("s1 + s2 + s3 is {}", s);
+    
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    // format! macro does not take ownership of any of its parameter.
+    let s = format!("{}-{}-{}", s1, s2, s3);
+    println!("s1 + s2 + s3 is {}", s);
+
+
+    // indexing into Strings
+    let s1 = String::from("Hello");
+    //let h = s1[0]; this is an error
+    //Rust string does not support indexing'
+
+    // string internal representation (interpretation)
+    // String is a wrapper over a Vec<u8>
+    let hello = String::from("Hola");
+    // in above case, len will be 4 and the vector storing the "Hola" is 4 bytes.
+    // each of letters takes 1 byte when encoded in UTF-8
+    let hello = String::from("Здравствуйте");
+    // in above case, let will be 24, not 12.
+    // each unicode scalar value in that string takes 2 bytes of storage.
+    // therefore an index into the string's bytes will not always correlate to a valid unicode scalar value.
+
+    // how to look at string from Rust
+    // Three ways. (bytes, scalar values, and grapheme clusters)
+    
+    let hello = String::from("Здравствуйте");
+    let s = &hello[0..4];
+    // type of the s here is &str
+    println!("{}", s);
+
+    for c in "नमस्ते".chars() {
+        println!("{}", c);
+    }
+
+    for c in "नमस्ते".bytes() {
+        println!("{}", c);
+    }
+
+    //Getting grapheme clusters from strings is complex, so this functionality is not provided by the standard library.
 }
